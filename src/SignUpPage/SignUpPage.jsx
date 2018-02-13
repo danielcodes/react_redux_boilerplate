57db8472b757423a9d76856eda25282302fdf8e9
Daniel Chia <daniel_1118@hotmail.com>
2018-02-12 19:47:40 -0500

    added sign up page

--- src/SignUpPage/SignUpPage.jsx
+++ src/SignUpPage/SignUpPage.jsx
@@ -2,8 +2,9 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
 
-import { BasePage } from '../BasePage';
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
 
+import { BasePage } from '../BasePage';
 import { userActions } from '../_actions';
 
 class SignUpPage extends React.Component {
@@ -45,40 +46,43 @@ class SignUpPage extends React.Component {
 		const { username, email, password, submitted } = this.state;
 		return (
 			<BasePage>
-				<h2>Sign Up</h2>
-
-				<form name="form" onSubmit={this.handleSubmit}>
-					<div className={'form-group' + (submitted && !username ? ' has-error' : '')}>
-						<label htmlFor="username">Username</label>
-						<input type="text" className="form-control" name="username" value={username} onChange={this.handleChange} />
-						{submitted && !username &&
-							<div className="help-block">Username is required</div>
-						}
-					</div>
-
-					<div className={'form-group' + (submitted && !email ? ' has-error' : '')}>
-						<label htmlFor="email">Email</label>
-						<input type="email" className="form-control" name="email" value={email} onChange={this.handleChange} />
-						{submitted && !email &&
-							<div className="help-block">Email is required</div>
-						}
-					</div>
-
-					<div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
-						<label htmlFor="password">Password</label>
-						<input type="password" className="form-control" name="password" value={password} onChange={this.handleChange} />
-						{submitted && !password &&
-								<div className="help-block">Password is required</div>
-						}
-					</div>
-
-					<div className="form-group">
-						<button className="btn btn-primary">Create account</button>
-						{loggingIn &&
-							<img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
-						}
-					</div>
-				</form>
+				<div className='login-form'>
+					<Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle'>
+						<Grid.Column style={{ maxWidth: 450 }}>
+							<Header as='h2' color='teal' textAlign='center'>
+								Sign up
+							</Header>
+							<Form size='large' onSubmit={this.handleSubmit}>
+								<Segment stacked>
+									<Form.Input fluid 
+										name='username' 
+										icon='user' 
+										iconPosition='left' 
+										placeholder='Username' 
+										onChange={this.handleChange} 
+									/>
+									<Form.Input fluid 
+										name='email' 
+										icon='address card' 
+										iconPosition='left' 
+										placeholder='Email' 
+										type='email'
+										onChange={this.handleChange} 
+									/>
+									<Form.Input fluid 
+										name='password' 
+										icon='lock' 
+										iconPosition='left' 
+										placeholder='Password' 
+										type='password' 
+										onChange={this.handleChange}
+									/>
+									<Button tye='submit' color='teal' fluid size='large'>Sign Up</Button>
+								</Segment>
+							</Form>
+						</Grid.Column>
+					</Grid>
+				</div>
 			</BasePage>
 		);
 	}
